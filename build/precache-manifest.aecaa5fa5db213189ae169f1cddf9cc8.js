83b0a3943e04884999d962ac25159faf55302a68
safarsafarov <safarov.safar@hotmail.com>
2020-10-25 22:30:21 +0800

    commit

---
+++ build/precache-manifest.aecaa5fa5db213189ae169f1cddf9cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4f792d00d3dfc678f369dae47d95a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a9502d525c63d55d67",
+    "url": "/static/css/main.1db9dad2.chunk.css"
+  },
+  {
+    "revision": "a41335c963ddefa63c94",
+    "url": "/static/js/2.72a9ac95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72a9ac95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a9502d525c63d55d67",
+    "url": "/static/js/main.a7219eeb.chunk.js"
+  },
+  {
+    "revision": "fb01dcf1aecacc333092",
+    "url": "/static/js/runtime-main.8f90365b.js"
+  }
+]);
